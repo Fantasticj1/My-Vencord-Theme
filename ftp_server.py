b8bd5ea58ebbdad5dd8f6985a20fc37706b01cd1
Fantasticj1 <98367824+Fantasticj1@users.noreply.github.com>
2025-05-24 19:28:57 -0500

    Update ftp_server.py

--- ftp_server.py
+++ ftp_server.py
@@ -1,13 +1,13 @@
-    from pyftpdlib.authorizers import DummyAuthorizer
-    from pyftpdlib.handlers import FTPHandler
-    from pyftpdlib.servers import FTPServer
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
 
-    authorizer = DummyAuthorizer()
-    authorizer.add_user('user', 'password', '.', perm='elradfmw') # user, password, home directory, permissions
-    authorizer.add_anonymous('.') # anonymous access, '.' is the current directory
+authorizer = DummyAuthorizer()
+authorizer.add_user('user', 'password', '.', perm='elradfmw') # user, password, home directory, permissions
+authorizer.add_anonymous('.') # anonymous access, '.' is the current directory
 
-    handler = FTPHandler
-    handler.authorizer = authorizer
+handler = FTPHandler
+handler.authorizer = authorizer
 
-    server = FTPServer(('0.0.0.0', 21), handler) # listen on all interfaces, port 21
-    server.serve_forever()
+server = FTPServer(('0.0.0.0', 21), handler) # listen on all interfaces, port 21
+server.serve_forever()
